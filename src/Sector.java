9eee005b24d1152997e1b6339aaaf24122384666
ALupo01 <amlupo01@gmail.com>
2024-06-08 20:37:19 -0300

    clase Sector

---
+++ src/Sector.java
@@ -0,0 +1,39 @@
+public class Sector {
+
+   private String identificador;
+   private int cantCamas;
+   private boolean reservable;
+   
+   public Sector (String identificador, int CantCamas, boolean reservable) {
+    
+    this.identificador = identificador;
+    this.cantCamas = cantCamas;
+    this.reservable = reservable;
+   }
+    
+   public String getIdentificador () {
+    return identificador;
+    
+   }
+   
+   public void setIdentificador () { 
+    this.identificador = identificador;
+   }
+   
+   public int getCantCamas () {
+    return cantCamas;
+   }
+   
+   public void setCantCamas () {
+    this.cantCamas = cantCamas;
+   }
+   
+   public boolean getReservable () {
+    return reservable; 
+   }
+   
+   public void setReservable () {
+    this.reservable = reservable;
+  }
+
+}
