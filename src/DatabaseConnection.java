1628fd69def3313a9b55c8a610eeb4cd527e8bb2
EmiFunes91 <emifunes8@gmail.com>
2024-06-09 04:31:29 -0300

    conexion con la base de datos

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/proyectoIntegradorUTN";
+    private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
     private static final String USER = "root";  // Cambia a tu usuario de MySQL
     private static final String PASSWORD = "";  // Cambia a tu contraseña de MySQL
 
