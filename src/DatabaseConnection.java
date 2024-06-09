bb89be95848569bf7d8da735ec21c44e14bbc3c4
EmiFunes91 <emifunes8@gmail.com>
2024-06-09 05:09:04 -0300

    se agrego el driver jar al archivo json - lib

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -4,10 +4,15 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
-    private static final String USER = "root";  // Cambia a tu usuario de MySQL
-    private static final String PASSWORD = "";  // Cambia a tu contraseña de MySQL
+    private static final String USER = "root";
+    private static final String PASSWORD = "your_password_here";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
