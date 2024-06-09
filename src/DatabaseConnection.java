fdf71f7da4a2d84daf77068524dcb551112d81f3
EmiFunes91 <emifunes8@gmail.com>
2024-06-09 05:30:42 -0300

    se ingreso pw de la base de datos para conectar

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "your_password_here";
+    private static final String PASSWORD = "36River449.";
 
     public static Connection getConnection() throws SQLException {
         try {
